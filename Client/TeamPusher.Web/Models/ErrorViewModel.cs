311411911c323acd68b585c610affe5f61f53a23
Maksym Kravchuk <malpinsoft@gmail.com>
2018-07-09 18:14:46 +0300

    init

---
+++ Client/TeamPusher.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeamPusher.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
